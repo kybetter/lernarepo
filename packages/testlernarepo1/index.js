97490b4c7e93a7dd5ca43ad5e7606333f6f969cd
zhaoliang <kybetter@163.com>
2019-04-04 15:36:28 +0800

    feat: 提一次提交

---
+++ packages/testlernarepo1/index.js
@@ -0,0 +1,3 @@
+import data from '@lernarepo/testlernarepo2';
+
+console.log(data);
---
+++ packages/testlernarepo2/index.js
@@ -0,0 +1,10 @@
+import {cloneDeep} from 'lodash';
+
+const obj = {aaa: 123};
+const ojb = cloneDeep(obj);
+
+
+export default {
+  name: 'this is @lernarepo/testlernarepo2',
+  data: ojb,
+}
